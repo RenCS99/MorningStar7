803b1d9f71234442c31150a3eca19acd8953b8ed
RenCS99 <65922620+RenCS99@users.noreply.github.com>
2020-11-27 21:10:50 -0800

    Renamed the database name from MorningStar7.db to morningstar.db. Change the SERVER URL to a XAMPP MySQL server. Merged the LocationData table, rowsAndSections table, and sync_info table with the barcode table. Renamed the remaining tables to lower case and without the term "_table" Modified the readFromLocalDatabase, saveToLocalDatabase, and updateLocalDatabase functions to better align with the new barcode table schema. Created a function named insertOneToBarcodeTable to test the sync between local and remote databases.

--- app/src/main/java/com/example/morningstar7/DataBaseHelper.java
+++ app/src/main/java/com/example/morningstar7/DataBaseHelper.java
@@ -13,23 +13,16 @@ import java.util.List;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
-    private static final String DB_NAME = "MorningStar7.db";
+    private static final String DB_NAME = "morningstar.db";
     private static final int DB_VERSION = 1;
     public static final int SYNC_STATUS_OK = 0;
     public static final int SYNC_STATUS_FAILED = 1;
-    public static final String SERVER_URL = "http://10.0.2.2/syncdemo/syncinfo.php";
+    public static final String SERVER_URL = "http://10.0.0.195/morningstar/syncinfo.php";
     public static final String UI_UPDATE_BROADCAST = "com.example.morningstar7.uiupdatebroadcast";
 
     // Database Tables
-    public static final String CREDENTIALS_TABLE = "CREDENTIALS_TABLE";
-    public static final String BARCODE_TABLE = "BARCODE_TABLE";
-    public static final String LOCATIONDATA_TABLE = "LOCATIONDATA_TABLE";
-    public static final String ROWSANDSECTIONS_TABLE = "ROWSANDSECTIONS_TABLE";
-    public static final String SYNC_INFO_TABLE = "SYNC_INFO";
-
-    // Sync's Columns
-    public static final String COLUMN_S_SCAN_ID = "s_scanId";
-    public static final String COLUMN_S_SYNC_STATUS = "s_syncStatus";
+    public static final String CREDENTIALS_TABLE = "credentials";
+    public static final String BARCODE_TABLE = "barcodes";
 
     // Credentials' Columns
     public static final String COLUMN_C_USER_ID = "c_userId";
@@ -40,52 +33,54 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_C_EMAIL = "c_email";
 
     // Barcode's Columns
-    public static final String COLUMN_B_CONTAINERID = "b_containerId";
     public static final String COLUMN_B_BARCODEID = "b_barcodeId";
     public static final String COLUMN_B_CONTAINERNAME = "b_containerName";
-    public static final String COLUMN_B_DIRTYBIT = "b_dirtyBit";
+    public static final String COLUMN_B_LATITUDE = "b_latitude";
+    public static final String COLUMN_B_LONGITUDE = "b_longitude";
+    public static final String COLUMN_B_ROW = "b_row";
+    public static final String COLUMN_B_SECTION = "b_section";
+    public static final String COLUMN_B_LASTUPDATED = "b_lastUpdated";
+    public static final String COLUMN_B_SYNCSTATUS = "b_syncStatus";
 
-    // LocationData's Columns
-    public static final String COLUMN_LD_CONTAINERID = "ld_containerId";
-    public static final String COLUMN_LD_LATITUDE = "ld_latitude";
-    public static final String COLUMN_LD_LONGITUDE = "ld_longitude";
-
-    // RowsAndSections' Table Columns
-    public static final String COLUMN_RS_CONTAINERID = "rs_containerId";
-    public static final String COLUMN_RS_ROW = "rs_row";
-    public static final String COLUMN_RS_SECTION = "rs_section";
 
     // Creating Tables SQL
     String createTable_credentials = "CREATE TABLE IF NOT EXISTS " + CREDENTIALS_TABLE + " (" + COLUMN_C_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_C_FIRSTNAME + " TEXT NOT NULL, " + COLUMN_C_LASTNAME + " TEXT NOT NULL, " + COLUMN_C_USERNAME + " Text NOT NULL, " + COLUMN_C_PASSWORD + " TEXT NOT NULL, " + COLUMN_C_EMAIL + " TEXT NOT NULL)";
-    String createTable_barcode = "CREATE TABLE IF NOT EXISTS " + BARCODE_TABLE + " (" + COLUMN_B_CONTAINERID + " INTEGER NOT NULL, " + COLUMN_B_BARCODEID + " INTEGER PRIMARY KEY, " + COLUMN_B_CONTAINERNAME + " TEXT NOT NULL, " + COLUMN_B_DIRTYBIT + " VARCHAR(1) NOT NULL)";
-    String createTable_locationdata = "CREATE TABLE IF NOT EXISTS " + LOCATIONDATA_TABLE + " (" + COLUMN_LD_CONTAINERID + " INTEGER PRIMARY KEY, " + COLUMN_LD_LATITUDE + " REAL NOT NULL, " + COLUMN_LD_LONGITUDE + " REAL NOT NULL)";
-    String createTable_rowsandsections = "CREATE TABLE IF NOT EXISTS " + ROWSANDSECTIONS_TABLE + " (" + COLUMN_RS_CONTAINERID + " INTEGER PRIMARY KEY, " + COLUMN_RS_ROW + " REAL NOT NULL, " + COLUMN_RS_SECTION + " REAL NOT NULL)";
-    String createTable_syncinfo = "CREATE TABLE IF NOT EXISTS " + SYNC_INFO_TABLE + " (" + COLUMN_S_SCAN_ID + " INTEGER PRIMARY KEY, " + COLUMN_S_SYNC_STATUS + " INTEGER NOT NULL)";
+    String createTable_barcode = "CREATE TABLE IF NOT EXISTS " + BARCODE_TABLE + " (" + COLUMN_B_BARCODEID + " VARCHAR(12) PRIMARY KEY, " + COLUMN_B_CONTAINERNAME + " TEXT NOT NULL, " + COLUMN_B_LATITUDE + " REAL NOT NULL, " + COLUMN_B_LONGITUDE + " REAL NOT NULL, " + COLUMN_B_ROW + " INTEGER NOT NULL, " + COLUMN_B_SECTION + " INTEGER NOT NULL, " + COLUMN_B_LASTUPDATED + " TEXT DEFAULT CURRENT_TIMESTAMP, " + COLUMN_B_SYNCSTATUS + " INTEGER NOT NULL)";
 
     public DataBaseHelper(@Nullable Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
-    public static Cursor readFromLocalDatabase(SQLiteDatabase db) {
-        String[] projection = {COLUMN_S_SCAN_ID, COLUMN_S_SYNC_STATUS};
+    public Cursor readFromLocalDatabase() {
+        String query = "SELECT * FROM " + BARCODE_TABLE + " WHERE b_syncStatus = 1;";
+        SQLiteDatabase db = this.getReadableDatabase();
 
-        return (db.query(SYNC_INFO_TABLE, projection, null, null, null, null, null));
+        Cursor cursor = null;
+        if(db != null)
+            cursor = db.rawQuery(query, null);
+
+        return cursor;
     }
 
-    public void saveToLocalDatabase(int scan_id, int sync_status, SQLiteDatabase db){
+    public void saveToLocalDatabase(String barcode_id, String container_name, double latitude, double longitude, int row, int section, String lastUpdated, int sync_status, SQLiteDatabase db){
         ContentValues cv = new ContentValues();
-        cv.put(COLUMN_S_SCAN_ID, scan_id);
-        cv.put(COLUMN_S_SYNC_STATUS,sync_status);
-        db.insert(SYNC_INFO_TABLE, null, cv);
+        cv.put(COLUMN_B_BARCODEID, barcode_id);
+        cv.put(COLUMN_B_CONTAINERNAME, container_name);
+        cv.put(COLUMN_B_LATITUDE, latitude);
+        cv.put(COLUMN_B_LONGITUDE, longitude);
+        cv.put(COLUMN_B_ROW, row);
+        cv.put(COLUMN_B_SECTION, section);
+        cv.put(COLUMN_B_LASTUPDATED, lastUpdated);
+        cv.put(COLUMN_B_SYNCSTATUS,sync_status);
+        db.insert(BARCODE_TABLE, null, cv);
     }
 
-    public void updateLocalDatabase(int scan_id, int sync_status,SQLiteDatabase db){
+    public void updateLocalDatabase(int barcode_id, int sync_status, SQLiteDatabase db){
         ContentValues cv = new ContentValues();
-        cv.put(COLUMN_S_SYNC_STATUS,sync_status);
-        String selection = COLUMN_S_SCAN_ID + " LIKE ?";
-        String id = String.valueOf(scan_id);
-        String[] args = {id};
-        db.update(SYNC_INFO_TABLE, cv, selection, args);
+        cv.put(COLUMN_B_SYNCSTATUS, sync_status);
+        String selection = COLUMN_B_BARCODEID + " LIKE ?";
+        String[] args = {String.valueOf(barcode_id)};
+        db.update(BARCODE_TABLE, cv, selection, args);
     }
 
     // This is called the first time a database is accessed. There should be code in here to create a new database.
@@ -93,9 +88,6 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(createTable_credentials);
         db.execSQL(createTable_barcode);
-        db.execSQL(createTable_locationdata);
-        db.execSQL(createTable_rowsandsections);
-        db.execSQL(createTable_syncinfo);
     }
 
     // This is called if the database version number changes. It prevents previous users apps from breaking when you change the database design.
@@ -104,8 +96,6 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         if(oldVersion < newVersion) {
             db.execSQL("DROP TABLE IF EXISTS '" + CREDENTIALS_TABLE + "'");
             db.execSQL("DROP TABLE IF EXISTS '" + BARCODE_TABLE + "'");
-            db.execSQL("DROP TABLE IF EXISTS '" + LOCATIONDATA_TABLE + "'");
-            db.execSQL("DROP TABLE IF EXISTS '" + ROWSANDSECTIONS_TABLE + "'");
             onCreate(db);
         }
     }
@@ -139,44 +129,34 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
-        cv.put(COLUMN_B_CONTAINERID, barcodeModel.getContainerId());
         cv.put(COLUMN_B_BARCODEID, barcodeModel.getBarcodeId());
         cv.put(COLUMN_B_CONTAINERNAME, barcodeModel.getContainerName());
-        cv.put(COLUMN_B_DIRTYBIT, barcodeModel.getDirtyBit());
+        cv.put(COLUMN_B_LATITUDE, barcodeModel.getLatitude());
+        cv.put(COLUMN_B_LONGITUDE, barcodeModel.getLongitude());
+        cv.put(COLUMN_B_ROW, barcodeModel.getRow());
+        cv.put(COLUMN_B_ROW, barcodeModel.getSection());
+        cv.put(COLUMN_B_LASTUPDATED, barcodeModel.getLastUpdated());
+        cv.put(COLUMN_B_SYNCSTATUS, barcodeModel.getSync_status());
 
         long insert = db.insert(CREDENTIALS_TABLE, null, cv);
 
         return insert != -1;
     }
 
-    //  Adding an entry to LocationData Table
-    public boolean addOneToLocationTable(LocationDataModel locationDataModel){
-
+    public void insertOneToBarcodeTable() {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues cv = new ContentValues();
+        String q = "SELECT * FROM " + BARCODE_TABLE + " WHERE b_barcodeId = ? OR b_barcodeId = ?;";
+        String val = "012345678905";
+        String[] args = {val};
+        Cursor cur = db.rawQuery(q, args);
 
-        cv.put(COLUMN_LD_CONTAINERID, locationDataModel.getContainerId());
-        cv.put(COLUMN_LD_LATITUDE, locationDataModel.getLatitude());
-        cv.put(COLUMN_LD_LONGITUDE, locationDataModel.getLongitude());
-
-        long insert = db.insert(LOCATIONDATA_TABLE, null, cv);
-
-        return insert != -1;
-    }
-
-    // Adding an entry to RowsAndSections Table
-    public boolean addOneToRowSectionsTable(RowsAndSectionsModel rowsAndSectionsModel){
-
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues cv = new ContentValues();
-
-        cv.put(COLUMN_RS_CONTAINERID, rowsAndSectionsModel.getContainerId());
-        cv.put(COLUMN_RS_ROW, rowsAndSectionsModel.getRow());
-        cv.put(COLUMN_RS_SECTION, rowsAndSectionsModel.getSection());
-
-        long insert = db.insert(CREDENTIALS_TABLE, null, cv);
-
-        return insert != -1;
+        if(cur.getCount() == 0) {
+            String q1 = "INSERT INTO " + BARCODE_TABLE + " VALUES('012345678905', 'Wooden Box', '12.45129', '-167.12746', '5', '1', '2020-11-27 07:05:12.215', '1');";
+            String q2 = "INSERT INTO " + BARCODE_TABLE + " VALUES('012345698905', 'Wooden Box', '12.45129', '-167.12746', '5', '1', '2020-11-27 07:05:12.215', '1');";
+            db.execSQL(q1);
+            db.execSQL(q2);
+        }
+        cur.close();
     }
 
     // Delete an entry from the Registration Table
@@ -205,47 +185,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         // If it is not found, return false
 
         SQLiteDatabase db = this.getWritableDatabase();
-        String queryString = "DELETE FROM " + BARCODE_TABLE + " WHERE " + COLUMN_B_CONTAINERID + " = " + barcodeModel.getBarcodeId();
-
-        Cursor cursor = db.rawQuery(queryString, null);
-
-        if (cursor.moveToFirst()) {
-            cursor.close();
-            return true;
-        }
-        else {
-            cursor.close();
-            return false;
-        }
-    }
-
-    // Delete an entry from the LocationData Table
-    public boolean deleteOneFromLDTable(LocationDataModel locationDataModel){
-        // Find userRegistrationModel in the database. If it is found, delete it and return true.
-        // If it is not found, return false
-
-        SQLiteDatabase db = this.getWritableDatabase();
-        String queryString = "DELETE FROM " + LOCATIONDATA_TABLE + " WHERE " + COLUMN_LD_CONTAINERID + " = " + locationDataModel.getContainerId();
-
-        Cursor cursor = db.rawQuery(queryString, null);
-
-        if (cursor.moveToFirst()) {
-            cursor.close();
-            return true;
-        }
-        else {
-            cursor.close();
-            return false;
-        }
-    }
-
-    // Delete an entry from the RowsAndSections Table
-    public boolean deleteOneFromRSTable(RowsAndSectionsModel rowsAndSectionsModel){
-        // Find userRegistrationModel in the database. If it is found, delete it and return true.
-        // If it is not found, return false
-
-        SQLiteDatabase db = this.getWritableDatabase();
-        String queryString = "DELETE FROM " + ROWSANDSECTIONS_TABLE + " WHERE " + COLUMN_RS_CONTAINERID + " = " + rowsAndSectionsModel.getContainerId();
+        String queryString = "DELETE FROM " + BARCODE_TABLE + " WHERE " + COLUMN_B_BARCODEID + " = " + barcodeModel.getBarcodeId();
 
         Cursor cursor = db.rawQuery(queryString, null);
 
@@ -269,49 +209,59 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         Cursor cur = db.rawQuery(queryString, args);
 
         if(cur.getCount() == 1){
+            cur.close();
             return true;
         }
         else{
+            cur.close();
             return false;
         }
+
     }
 
-//    public List<SyncEntry> getEveryEntryToSync(){
-//        List<SyncEntry> returnList = new ArrayList<>();
-//    }
-
-    public List<UserRegistrationModel> getEveryoneRegistered(){
-        List<UserRegistrationModel> returnList = new ArrayList<>();
-
-        // Get data from the database
-
-        String queryString = "SELECT * FROM " + CREDENTIALS_TABLE;
-
+    public Cursor readAllDataFromBarcodeTable() {
+        String query = "SELECT b_barcodeId, b_row, b_section, b_syncStatus FROM " + BARCODE_TABLE + " WHERE c_syncStatus = 1;";
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.rawQuery(queryString, null);
+        Cursor cursor = null;
+        if(db != null)
+            cursor = db.rawQuery(query, null);
 
-        if(cursor.moveToFirst()) {
-            // Loop through the cursor (result set) and create new user objects. Put them into the return list.
-
-            do {
-                String userFirstName = cursor.getString(1);
-                String userLastName = cursor.getString(2);
-                String userUsername = cursor.getString(3);
-                String userPassword = cursor.getString(4);
-                String userEmail = cursor.getString(5);
-
-                UserRegistrationModel newUserRegistrationModel = new UserRegistrationModel(userFirstName, userLastName, userUsername, userPassword, userEmail);
-                returnList.add(newUserRegistrationModel);
-
-
-            } while(cursor.moveToNext());
-
-        }
-
-        // Close both the cursor and the db when done.
-        cursor.close();
-        db.close();
-        return returnList;
+        return cursor;
     }
+
+//    public List<UserRegistrationModel> getEveryoneRegistered(){
+//        List<UserRegistrationModel> returnList = new ArrayList<>();
+//
+//        // Get data from the database
+//
+//        String queryString = "SELECT * FROM " + CREDENTIALS_TABLE;
+//
+//        SQLiteDatabase db = this.getReadableDatabase();
+//
+//        Cursor cursor = db.rawQuery(queryString, null);
+//
+//        if(cursor.moveToFirst()) {
+//            // Loop through the cursor (result set) and create new user objects. Put them into the return list.
+//
+//            do {
+//                String userFirstName = cursor.getString(1);
+//                String userLastName = cursor.getString(2);
+//                String userUsername = cursor.getString(3);
+//                String userPassword = cursor.getString(4);
+//                String userEmail = cursor.getString(5);
+//
+//                UserRegistrationModel newUserRegistrationModel = new UserRegistrationModel(userFirstName, userLastName, userUsername, userPassword, userEmail);
+//                returnList.add(newUserRegistrationModel);
+//
+//
+//            } while(cursor.moveToNext());
+//
+//        }
+//
+//        // Close both the cursor and the db when done.
+//        cursor.close();
+//        db.close();
+//        return returnList;
+//    }
 }
