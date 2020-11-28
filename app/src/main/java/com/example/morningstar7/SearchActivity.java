ae0b9b2d973504e2f3ab747a20a350fe406af98c
Puneet Pal Kaur <pkaur34@ucmerced.edu>
2020-11-27 23:53:12 -0800

    implemented search button with sample data

--- app/src/main/java/com/example/morningstar7/SearchActivity.java
+++ app/src/main/java/com/example/morningstar7/SearchActivity.java
@@ -1,12 +1,24 @@
 package com.example.morningstar7;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.view.MenuItemCompat;
 import android.os.Bundle;
-
-
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 public class SearchActivity extends AppCompatActivity{
+    ListView listView;
+    ArrayList<String> stringArrayList = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +28,48 @@ public class SearchActivity extends AppCompatActivity{
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        listView = findViewById(R.id.list_view);
+
+        for(int i=0; i<=100; i++) {
+            stringArrayList.add("Item " + i);
+        }
+
+
+        adapter = new ArrayAdapter(SearchActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.countries_array));
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), adapter.getItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_search, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_view);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
