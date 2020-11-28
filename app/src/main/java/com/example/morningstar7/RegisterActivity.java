6c41d04d239aaf37f5ebde32bf26fd13e3bc57b7
RenCS99 <65922620+RenCS99@users.noreply.github.com>
2020-11-27 21:51:09 -0800

    Modified the RegisterActivity.java to send the registration info to the remote database first and if it fails it sends it to the local database.

--- app/src/main/java/com/example/morningstar7/RegisterActivity.java
+++ app/src/main/java/com/example/morningstar7/RegisterActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,13 +14,15 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
 public class RegisterActivity extends AppCompatActivity {
 
     // References to buttons and other controls on the layout
-    Button btn_register, btn_showAll;
-    EditText et_FirstName, et_LastName, et_Username, et_Password, et_EmailAddress;
-    ListView lv_registeredUsers;
-    ArrayAdapter userArrayAdapter;
+    Button btn_register; //, btn_showAll;
+    EditText et_FirstName, et_LastName, et_Usernamesignup, et_Passwordsignup, et_EmailAddress;
+//    ListView lv_registeredUsers;
+//    ArrayAdapter userArrayAdapter;
     DataBaseHelper dataBaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,80 +30,93 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.register_interface);
 
         btn_register = findViewById(R.id.btn_register);
-        btn_showAll = findViewById(R.id.btn_showAll);
         et_FirstName = findViewById(R.id.et_FirstName);
         et_LastName = findViewById(R.id.et_LastName);
-        et_Username = findViewById(R.id.et_Username);
-        et_Password = findViewById(R.id.et_Password);
+        et_Usernamesignup = findViewById(R.id.et_Usernamesignup);
+        et_Passwordsignup = findViewById(R.id.et_Passwordsignup);
         et_EmailAddress = findViewById(R.id.et_EmailAddress);
-        lv_registeredUsers = findViewById(R.id.lv_registeredUsers);
 
         dataBaseHelper = new DataBaseHelper(RegisterActivity.this);
 
-        //Shows Registered Users
-        //ShowRegisteredUserOnListView(dataBaseHelper);
-
         // Button Listeners for the add and view all buttons
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
+        btn_register.setOnClickListener(v -> {
+            final String firstname, lastname, username, password, email;
+            firstname = String.valueOf(et_FirstName.getText());
+            lastname = String.valueOf(et_LastName.getText());
+            username = String.valueOf(et_Usernamesignup.getText());
+            password = String.valueOf(et_Passwordsignup.getText());
+            email = String.valueOf(et_EmailAddress.getText());
 
-                UserRegistrationModel userRegistrationModel;
+            if(!firstname.equals("") && !lastname.equals("") && !username.equals("") && !password.equals("") && !email.equals("")) {
+                Handler handler = new Handler(Looper.getMainLooper());
+                //End Write and Read data with URL
+                handler.post(() -> {
+                    //Starting Write and Read data with URL
+                    //Creating array for parameters
+                    String[] field = new String[5];
+                    field[0] = "c_firstname";
+                    field[1] = "c_lastname";
+                    field[2] = "c_username";
+                    field[3] = "c_password";
+                    field[4] = "c_email";
+                    //Creating array for data
+                    String[] data = new String[5];
+                    data[0] = firstname;
+                    data[1] = lastname;
+                    data[2] = username;
+                    data[3] = password;
+                    data[4] = email;
+                    PutData putData = new PutData("http://10.0.0.195/morningstar/signup.php", "POST", field, data);
+                    if (putData.startPut()) {
+                        if (putData.onComplete()) {
+                            String result = putData.getResult();
+                            if (result.equals("Registration Successful")) {
+                                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                finish();
+                            } else {
+                                UserRegistrationModel userRegistrationModel;
 
-                if(TextUtils.isEmpty(et_FirstName.getText()) || TextUtils.isEmpty(et_LastName.getText()) || TextUtils.isEmpty(et_Username.getText()) || TextUtils.isEmpty(et_Password.getText()) || TextUtils.isEmpty(et_EmailAddress.getText())){
-                    Toast.makeText(RegisterActivity.this, "All Fields Required.", Toast.LENGTH_SHORT).show();
-                    et_FirstName.setText("");
-                    et_LastName.setText("");
-                    et_Username.setText("");
-                    et_Password.setText("");
-                    et_EmailAddress.setText("");
-                } else{
-                    userRegistrationModel = new UserRegistrationModel(et_FirstName.getText().toString(), et_LastName.getText().toString(), et_Username.getText().toString(), et_Password.getText().toString(), et_EmailAddress.getText().toString());
-                    boolean success = dataBaseHelper.addOneToRegTable(userRegistrationModel);
+                                userRegistrationModel = new UserRegistrationModel(et_FirstName.getText().toString(), et_LastName.getText().toString(), et_Usernamesignup.getText().toString(), et_Passwordsignup.getText().toString(), et_EmailAddress.getText().toString());
+                                boolean success = dataBaseHelper.addOneToRegTable(userRegistrationModel);
 
-                    if (success) {
-                        Toast.makeText(RegisterActivity.this, userRegistrationModel.toString(), Toast.LENGTH_SHORT).show();
-                        Toast.makeText(RegisterActivity.this, "Success = " + success, Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(RegisterActivity.this, SelectionActivity.class));
+                                if (success) {
+                                    //Toast.makeText(RegisterActivity.this, userRegistrationModel.toString(), Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(RegisterActivity.this, "Success = " + success, Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegisterActivity.this, SelectionActivity.class));
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "Username Already Taken.", Toast.LENGTH_SHORT).show();
+                                    et_FirstName.setText("");
+                                    et_LastName.setText("");
+                                    et_Usernamesignup.setText("");
+                                    et_Passwordsignup.setText("");
+                                    et_EmailAddress.setText("");
+                                }
+                            }
+                            //Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                        }
                     }
-                    else {
-                        Toast.makeText(RegisterActivity.this, "Username Already Taken.", Toast.LENGTH_SHORT).show();
-                        et_FirstName.setText("");
-                        et_LastName.setText("");
-                        et_Username.setText("");
-                        et_Password.setText("");
-                        et_EmailAddress.setText("");
-                    }
-                }
+                });
             }
+            else {
+                Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
+            }
+
+
         });
 
-        btn_showAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                DataBaseHelper dataBaseHelper = new DataBaseHelper(RegisterActivity.this);
-
-                ShowRegisteredUserOnListView(dataBaseHelper);
-
-                //Toast.makeText(MainActivity.this, everyone.toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-//        lv_registeredUsers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//        btn_showAll.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-//                UserRegistrationModel clickedUser = (UserRegistrationModel) parent.getItemAtPosition(position);
-//                dataBaseHelper.deleteOneFromRegTable(clickedUser);
+//            public void onClick(View v) {
+//                DataBaseHelper dataBaseHelper = new DataBaseHelper(RegisterActivity.this);
 //                ShowRegisteredUserOnListView(dataBaseHelper);
-//                Toast.makeText(MainActivity.this, "Deleted " + clickedUser, Toast.LENGTH_SHORT).show();
+//                lv_registeredUsers.setVisibility(lv_registeredUsers.isShown() ? lv_registeredUsers.GONE : lv_registeredUsers.VISIBLE);
 //            }
 //        });
-
     }
 
-    private void ShowRegisteredUserOnListView(DataBaseHelper dataBaseHelper2) {
-        userArrayAdapter = new ArrayAdapter<>(RegisterActivity.this, android.R.layout.simple_list_item_1, dataBaseHelper2.getEveryoneRegistered());
-        lv_registeredUsers.setAdapter(userArrayAdapter);
-    }
+//    private void ShowRegisteredUserOnListView(DataBaseHelper dataBaseHelper2) {
+//        userArrayAdapter = new ArrayAdapter<>(RegisterActivity.this, android.R.layout.simple_list_item_1, dataBaseHelper2.getEveryoneRegistered());
+//        lv_registeredUsers.setAdapter(userArrayAdapter);
+//    }
 }
