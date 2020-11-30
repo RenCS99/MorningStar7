f6cedc8d4d38e605f5cc200fda2292245ce37ee1
RenCS99 <65922620+RenCS99@users.noreply.github.com>
2020-11-29 22:54:45 -0800

    Implemented the syncing of credentials between local and remote database. When user logs in the data from the credentials table in the remote database gets fetch and then the information that is not already in the local database gets added into it. This allows for the user to be able to log in even if there is no connection.

--- app/src/main/java/com/example/morningstar7/LoginActivity.java
+++ app/src/main/java/com/example/morningstar7/LoginActivity.java
@@ -1,25 +1,28 @@
 package com.example.morningstar7;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.StrictMode;
-import android.text.TextUtils;
-import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
-import java.sql.Connection;
-import java.sql.Statement;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -64,10 +67,15 @@ public class LoginActivity extends AppCompatActivity {
                         if (putData.onComplete()) {
                             String result = putData.getResult();
                             if(result.equals("Login Success")) {
-                                dataBaseHelper.checkIfEntryInDb(userRegistrationModel);
+                                boolean b = dataBaseHelper.checkIfEntryInDb(userRegistrationModel);
+                                if(b) {
                                 Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
                                 startActivity(new Intent(getApplicationContext(), SelectionActivity.class));
-                                finish();
+                                }
+                                else {
+                                    new Connection().execute();
+                                    startActivity(new Intent(getApplicationContext(), SelectionActivity.class));
+                                }
                             }
                             else {
 
@@ -92,13 +100,65 @@ public class LoginActivity extends AppCompatActivity {
             else {
                 Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_LONG).show();
             }
-
-
-
-
-
         });
 
         btn_register.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
+    }
+
+    class Connection extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = "";
+            String host = "http://10.0.0.195/morningstar/getCreds.php";
+            try {
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                request.setURI(new URI(host));
+                HttpResponse response = client.execute(request);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+                StringBuffer stringBuffer = new StringBuffer("");
+
+                String line = "";
+                while ((line = reader.readLine()) != null){
+                    stringBuffer.append(line);
+                    break;
+                }
+                reader.close();
+                result = stringBuffer.toString();
+
+            }
+            catch (Exception e) {
+                return new String("Exception: " + e.getMessage());
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                int success = jsonObject.getInt("success");
+                if(success == 1){
+                    JSONArray users = jsonObject.getJSONArray("users");
+                    for(int i = 0; i < users.length(); i++) {
+                        JSONObject user = users.getJSONObject(i);
+                        UserRegistrationModel userRegistrationModel2 = new UserRegistrationModel(user.getString("c_firstname"), user.getString("c_lastname"), user.getString("c_username"), user.getString("c_password"), user.getString("c_email"));
+                        boolean b = dataBaseHelper.addOneToRegTable(userRegistrationModel2);
+                        if (b) {
+                            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "There is no users yet", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+            Toast.makeText(getApplicationContext(), "Not passed", Toast.LENGTH_LONG).show();
+        }
     }
 }
