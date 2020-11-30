017f1b304786f713e17626b94a0516661eb92a11
RenCS99 <65922620+RenCS99@users.noreply.github.com>
2020-11-30 14:21:00 -0800

    Fix an issue which made the app crash because it wouldn't get a response from the server. Therefore, a function was added to check the connection between the app and the server. This function takes in a URL and a timeout and proceeds to check for the connection and once the timeout is reach it resorts to the local database in the phone. Added a try and catch statement to prevent the app from crashing when trying to connect to the MySQL server and the connection is refused.

--- app/src/main/java/com/example/morningstar7/LoginActivity.java
+++ app/src/main/java/com/example/morningstar7/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.telecom.DisconnectCause;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -22,6 +23,8 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -35,7 +38,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_interface);
         getSupportActionBar().hide();
-
+        //String url = "http://127.0.0.1/morningstar/DbConnect.php";
         btn_login = findViewById(R.id.btn_login);
         btn_register = findViewById(R.id.btn_register);
         et_usernamelogin = findViewById(R.id.et_usernamelogin);
@@ -46,70 +49,86 @@ public class LoginActivity extends AppCompatActivity {
         btn_login.setOnClickListener(v -> {
             UserRegistrationModel userRegistrationModel;
             userRegistrationModel = new UserRegistrationModel(null, null, et_usernamelogin.getText().toString(), et_passwordlogin.getText().toString(), null);
+            boolean b = dataBaseHelper.checkIfEntryInDb(userRegistrationModel);
+            //boolean t = isConnectedToServer(url,120);
+            //Toast.makeText(getApplicationContext(), " " + t, Toast.LENGTH_SHORT).show();
+            //if(!t) {
 
-            final String un, pass;
-            un = String.valueOf(et_usernamelogin.getText());
-            pass = String.valueOf(et_passwordlogin.getText());
-            if(!un.equals("") && !pass.equals("")) {
-                Handler handler = new Handler(Looper.getMainLooper());
-                handler.post(() -> {
-                    //Starting Write and Read data with URL
-                    //Creating array for parameters
-                    String[] field = new String[2];
-                    field[0] = "c_username";
-                    field[1] = "c_password";
-                    //Creating array for data
-                    String[] data = new String[2];
-                    data[0] = un;
-                    data[1] = pass;
-                    PutData putData = new PutData("http://10.0.0.195/morningstar/login.php", "POST", field, data);
-                    if (putData.startPut()) {
-                        if (putData.onComplete()) {
-                            String result = putData.getResult();
-                            if(result.equals("Login Success")) {
-                                boolean b = dataBaseHelper.checkIfEntryInDb(userRegistrationModel);
-                                if(b) {
-                                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
-                                startActivity(new Intent(getApplicationContext(), SelectionActivity.class));
-                                }
-                                else {
-                                    new Connection().execute();
-                                    startActivity(new Intent(getApplicationContext(), SelectionActivity.class));
-                                }
-                            }
-                            else {
+                final String un, pass;
+                un = String.valueOf(et_usernamelogin.getText());
+                pass = String.valueOf(et_passwordlogin.getText());
+                if (!un.equals("") && !pass.equals("")) {
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(() -> {
+                        //Starting Write and Read data with URL
+                        //Creating array for parameters
+                        String[] field = new String[2];
+                        field[0] = "c_username";
+                        field[1] = "c_password";
+                        //Creating array for data
+                        String[] data = new String[2];
+                        data[0] = un;
+                        data[1] = pass;
+                        try {
+                            PutData putData = new PutData("http://10.0.0.195/morningstar/login.php", "POST", field, data);
 
-                                boolean b = dataBaseHelper.checkIfEntryInDb(userRegistrationModel);
-
-                                if(b){
-                                    Toast.makeText(LoginActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(LoginActivity.this, SelectionActivity.class));
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    if (result.equals("Login Success")) {
+                                        if (b) {
+                                            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                            startActivity(new Intent(getApplicationContext(), SelectionActivity.class));
+                                        } else {
+                                            new Connection().execute();
+                                            startActivity(new Intent(getApplicationContext(), SelectionActivity.class));
+                                        }
+                                    } else {
+                                        if (b) {
+                                            Toast.makeText(LoginActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(LoginActivity.this, SelectionActivity.class));
+                                        } else {
+                                            Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                                            et_usernamelogin.setText("");
+                                            et_passwordlogin.setText("");
+                                        }
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                    }
                                 }
-                                else {
-                                    Toast.makeText(LoginActivity.this, "No Existing User.", Toast.LENGTH_SHORT).show();
-                                    et_usernamelogin.setText("");
-                                    et_passwordlogin.setText("");
-                                }
-                                //Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
                             }
                         }
-                    }
-                    //End Write and Read data with URL
-                });
-            }
-            else {
-                Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_LONG).show();
-            }
+                        catch (Exception e){
+                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                        //End Write and Read data with URL
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_LONG).show();
+                }
+
+//            }
+//            else {
+//                boolean b = dataBaseHelper.checkIfEntryInDb(userRegistrationModel);
+//
+//                if (b) {
+//                    Toast.makeText(LoginActivity.this, "Log", Toast.LENGTH_SHORT).show();
+//                    startActivity(new Intent(LoginActivity.this, SelectionActivity.class));
+//                } else {
+//                    Toast.makeText(LoginActivity.this, "No Existing User.", Toast.LENGTH_SHORT).show();
+//                    et_usernamelogin.setText("");
+//                    et_passwordlogin.setText("");
+//                }
+//            }
         });
 
         btn_register.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
     }
 
-    class Connection extends AsyncTask<String, String, String> {
+    public class Connection extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... strings) {
-            String result = "";
+            String result;
             String host = "http://10.0.0.195/morningstar/getCreds.php";
             try {
                 HttpClient client = new DefaultHttpClient();
@@ -118,9 +137,9 @@ public class LoginActivity extends AppCompatActivity {
                 HttpResponse response = client.execute(request);
                 BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
-                StringBuffer stringBuffer = new StringBuffer("");
+                StringBuilder stringBuffer = new StringBuilder();
 
-                String line = "";
+                String line;
                 while ((line = reader.readLine()) != null){
                     stringBuffer.append(line);
                     break;
@@ -130,7 +149,7 @@ public class LoginActivity extends AppCompatActivity {
 
             }
             catch (Exception e) {
-                return new String("Exception: " + e.getMessage());
+                return "Exception: " + e.getMessage();
             }
 
             return result;
@@ -150,6 +169,7 @@ public class LoginActivity extends AppCompatActivity {
                         if (b) {
                             Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
                         }
+
                     }
                 }
                 else {
@@ -159,6 +179,18 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
             }
             Toast.makeText(getApplicationContext(), "Not passed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean isConnectedToServer(String url, int timeout) {
+        try{
+            URL myUrl = new URL(url);
+            URLConnection connection = myUrl.openConnection();
+            connection.setConnectTimeout(timeout);
+            connection.connect();
+            return true;
+        } catch (Exception e){
+            return false;
         }
     }
 }
