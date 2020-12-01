d58956d78c0727f20dfd52c90e73f6b85acdcc72
RenCS99 <65922620+RenCS99@users.noreply.github.com>
2020-11-30 16:49:56 -0800

    Deleted unnecessary imports

--- app/src/main/java/com/example/morningstar7/DataBaseHelper.java
+++ app/src/main/java/com/example/morningstar7/DataBaseHelper.java
@@ -5,12 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
-
 import androidx.annotation.Nullable;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
