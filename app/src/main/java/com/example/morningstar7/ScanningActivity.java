08f26cb6ab55fb3e55734b42dd8211560d732e83
James Wei <James.wei.cs@gmail.com>
2020-12-02 16:56:48 -0800

    new merger from work done on diff workspace

--- app/src/main/java/com/example/morningstar7/ScanningActivity.java
+++ app/src/main/java/com/example/morningstar7/ScanningActivity.java
@@ -17,49 +17,5 @@ import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.zxing.Result;
 
 
-public class ScanningActivity extends AppCompatActivity{
-    private CodeScanner mCodeScanner;
+//new  merger from different branch
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.scanning_interface);
-        getSupportActionBar().setTitle("Scan");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        CodeScannerView scannerView = findViewById(R.id.scanner_view);
-        mCodeScanner = new CodeScanner(this, scannerView);
-        mCodeScanner.setDecodeCallback(new DecodeCallback() {
-            @Override
-            public void onDecoded(@NonNull final Result result) {
-                ScanningActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(ScanningActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-        scannerView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCodeScanner.startPreview();
-            }
-        });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mCodeScanner.startPreview();
-    }
-
-    @Override
-    protected void onPause() {
-        mCodeScanner.releaseResources();
-        super.onPause();
-    }
-
-
-
-}
