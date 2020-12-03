53ff631e6c8012c21268cb5d346562739a7301e7
jesusvera <geezus918948@gmail.com>
2020-12-02 16:42:27 -0800

    get the correct cols and rows, barcode id to read into user submission

--- app/src/main/java/com/example/morningstar7/RowColumnBarcodeModel.java
+++ app/src/main/java/com/example/morningstar7/RowColumnBarcodeModel.java
@@ -23,6 +23,36 @@ public class RowColumnBarcodeModel {
         this.column = column;
         this.barcode = barcode;
     }
+    public int getRow() {
+        return row;
+    }
 
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
 }
