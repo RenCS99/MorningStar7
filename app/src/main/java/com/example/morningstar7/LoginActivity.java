b65bbccb4d7bb40f0d51d5dc145de54b22555164
RenCS99 <65922620+RenCS99@users.noreply.github.com>
2020-12-04 15:25:00 -0800

    Deleted some unnecessary code.

--- app/src/main/java/com/example/morningstar7/LoginActivity.java
+++ app/src/main/java/com/example/morningstar7/LoginActivity.java
@@ -6,7 +6,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.telecom.DisconnectCause;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,12 +18,9 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -38,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_interface);
         getSupportActionBar().hide();
-        //String url = "http://127.0.0.1/morningstar/DbConnect.php";
+
         btn_login = findViewById(R.id.btn_login);
         btn_register = findViewById(R.id.btn_register);
         et_usernamelogin = findViewById(R.id.et_usernamelogin);
@@ -50,75 +46,59 @@ public class LoginActivity extends AppCompatActivity {
             UserRegistrationModel userRegistrationModel;
             userRegistrationModel = new UserRegistrationModel(null, null, et_usernamelogin.getText().toString(), et_passwordlogin.getText().toString(), null);
             boolean b = dataBaseHelper.checkIfEntryInDb(userRegistrationModel);
-            //boolean t = isConnectedToServer(url,120);
-            //Toast.makeText(getApplicationContext(), " " + t, Toast.LENGTH_SHORT).show();
-            //if(!t) {
 
-                final String un, pass;
-                un = String.valueOf(et_usernamelogin.getText());
-                pass = String.valueOf(et_passwordlogin.getText());
-                if (!un.equals("") && !pass.equals("")) {
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    handler.post(() -> {
-                        //Starting Write and Read data with URL
-                        //Creating array for parameters
-                        String[] field = new String[2];
-                        field[0] = "c_username";
-                        field[1] = "c_password";
-                        //Creating array for data
-                        String[] data = new String[2];
-                        data[0] = un;
-                        data[1] = pass;
-                        try {
-                            PutData putData = new PutData("http://10.0.0.195/morningstar/login.php", "POST", field, data);
+            final String un, pass;
+            un = String.valueOf(et_usernamelogin.getText());
+            pass = String.valueOf(et_passwordlogin.getText());
+            if (!un.equals("") && !pass.equals("")) {
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(() -> {
+                    //Starting Write and Read data with URL
+                    //Creating array for parameters
+                    String[] field = new String[2];
+                    field[0] = "c_username";
+                    field[1] = "c_password";
+                    //Creating array for data
+                    String[] data = new String[2];
+                    data[0] = un;
+                    data[1] = pass;
+                    try {
+                        PutData putData = new PutData("http://10.0.0.195/morningstar/login.php", "POST", field, data);
 
-                            if (putData.startPut()) {
-                                if (putData.onComplete()) {
-                                    String result = putData.getResult();
-                                    if (result.equals("Login Success")) {
-                                        if (b) {
-                                            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
-                                            startActivity(new Intent(getApplicationContext(), SelectionActivity.class));
-                                        } else {
-                                            new Connection().execute();
-                                            startActivity(new Intent(getApplicationContext(), SelectionActivity.class));
-                                        }
-                                    } else {
-                                        if (b) {
-                                            Toast.makeText(LoginActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
-                                            startActivity(new Intent(LoginActivity.this, SelectionActivity.class));
-                                        } else {
-                                            Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
-                                            et_usernamelogin.setText("");
-                                            et_passwordlogin.setText("");
-                                        }
+                        if (putData.startPut()) {
+                            if (putData.onComplete()) {
+                                String result = putData.getResult();
+                                if (result.equals("Login Success")) {
+                                    if (b) {
                                         Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                        startActivity(new Intent(getApplicationContext(), SelectionActivity.class));
+                                    } else {
+                                        new Connection().execute();
+                                        startActivity(new Intent(getApplicationContext(), SelectionActivity.class));
                                     }
+                                } else {
+                                    if (b) {
+                                        Toast.makeText(LoginActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this, SelectionActivity.class));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                                        et_usernamelogin.setText("");
+                                        et_passwordlogin.setText("");
+                                    }
+                                    Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
                                 }
                             }
                         }
-                        catch (Exception e){
-                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                        //End Write and Read data with URL
-                    });
-                } else {
-                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_LONG).show();
-                }
-
-//            }
-//            else {
-//                boolean b = dataBaseHelper.checkIfEntryInDb(userRegistrationModel);
-//
-//                if (b) {
-//                    Toast.makeText(LoginActivity.this, "Log", Toast.LENGTH_SHORT).show();
-//                    startActivity(new Intent(LoginActivity.this, SelectionActivity.class));
-//                } else {
-//                    Toast.makeText(LoginActivity.this, "No Existing User.", Toast.LENGTH_SHORT).show();
-//                    et_usernamelogin.setText("");
-//                    et_passwordlogin.setText("");
-//                }
-//            }
+                    }
+                    catch (Exception e){
+                        // ERROR Code 2002: WAIT_FAILURE_SERVER
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    //End Write and Read data with URL
+                });
+            } else {
+                Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_LONG).show();
+            }
         });
 
         btn_register.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
@@ -167,9 +147,8 @@ public class LoginActivity extends AppCompatActivity {
                         UserRegistrationModel userRegistrationModel2 = new UserRegistrationModel(user.getString("c_firstname"), user.getString("c_lastname"), user.getString("c_username"), user.getString("c_password"), user.getString("c_email"));
                         boolean b = dataBaseHelper.addOneToRegTable(userRegistrationModel2);
                         if (b) {
-                            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "Credentials Synced", Toast.LENGTH_LONG).show();
                         }
-
                     }
                 }
                 else {
@@ -178,19 +157,7 @@ public class LoginActivity extends AppCompatActivity {
             } catch (JSONException e) {
                 Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
             }
-            Toast.makeText(getApplicationContext(), "Not passed", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public boolean isConnectedToServer(String url, int timeout) {
-        try{
-            URL myUrl = new URL(url);
-            URLConnection connection = myUrl.openConnection();
-            connection.setConnectTimeout(timeout);
-            connection.connect();
-            return true;
-        } catch (Exception e){
-            return false;
+            //Toast.makeText(getApplicationContext(), "Not passed", Toast.LENGTH_LONG).show();
         }
     }
 }
